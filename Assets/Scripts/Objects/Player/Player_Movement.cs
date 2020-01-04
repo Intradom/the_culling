29cc22c2dc6f1dad644df30d73d7bac28ce6c4f8
Bo <zboyang@umich.edu>
2020-01-03 20:16:39 -0500

    Added Player movement and basic arena

---
+++ Assets/Scripts/Objects/Player/Player_Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [SerializeField] private float player_speed = 0;
+
+    private Rigidbody2D ref_rbody;
+
+    private void Awake()
+    {
+        ref_rbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Do movements in fixed update for smoother movements
+        float movement_hori = Input.GetAxis("Horizontal");
+        float movement_vert = Input.GetAxis("Vertical");
+
+        // Use forces to apply acceleration
+        float time_scale = Time.fixedDeltaTime * player_speed;
+        ref_rbody.AddForce(new Vector2(movement_hori * time_scale, movement_vert * time_scale));
+    }
+}
